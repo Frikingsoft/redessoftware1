4e64173058f7f27ef40e6f5e699fc39016a30b8e
frikingsoft <nicolas.barone1@gmail.com>
2026-05-21 18:44:58 -0300

    rutas post

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ import { usuarios } from "./bd.js"
 router.get("/",(req,res)=>{
      res.render("index.hbs",{usuarios})
 })
+router.post("/usuario/:id",async(req,res)=>{
+     let buscar_usuario = await usuarios.find(usuario => usuario.cedula === req.params.id) 
+     res.send(buscar_usuario)
+     //res.render("index.hbs",{usuarios})
+
+})
