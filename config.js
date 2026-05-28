4b2a306e4ad3615749d914d9da31d2cb02ddcd2f
frikingsoft <nicolas.barone1@gmail.com>
2026-05-28 18:38:45 -0300

    cambios 28-5-2026

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 import express from "express" // LLamamos al modulo de express
 import { fileURLToPath } from "url" // para obtener las rutas del archivo actual
 import { dirname } from "path" // para obtener las rutas del directorio actual
-import  hbs  from "hbs"
+import  hbs  from "hbs" // LLamamos al modulo de hbs para las plantillas 
 const __filename = fileURLToPath(import.meta.url)// para obtener las rutas del archivo actual
 const __dirname = dirname(__filename) // para obtener las rutas del directorio actual
 
-const ruta = `${__dirname}/views`
+const ruta = `${__dirname}/views` // ruta que voy a utilizar para los archivos estaticos
 const servidor = express() // le paso la configuracion del servidor a la variable servidor
 servidor.listen(4000)//El puerto del servidor
 servidor.use(express.json())// configuramos el servidor para trabajar con json
 servidor.use(express.static(ruta))//definimos las rutas estaticas
-servidor.set("view engine","hbs")
-hbs.registerPartials(`${__dirname}/views/partials`)
-const router = express.Router()
-servidor.use(router)
+servidor.set("view engine","hbs")// utilizamos hbs como motor de plantillas para darle vitaminas al html
+hbs.registerPartials(`${__dirname}/views/partials`) // registramos los parciales que se van a utilizar en mas de una pagina de la aplicacion
+const router = express.Router() // creamos una variable de router para crear las rutas de la aplicacion
+servidor.use(router)// le avisamos a express que use el router
 
 export {
     router,
     ruta,
     servidor
-}
+}// exportamos los siguientes objetos
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import { router,ruta} from "./config.js"
-import { usuarios } from "./bd.js"
+import { router,ruta} from "./config.js" // llamamos a los objetos router y ruta creados en config.js
+import { usuarios } from "./bd.js" //llamamos al objeto usuarios que esta en bd.js y que simula los usuarios de una base de datos 
 
 router.get("/",(req,res)=>{
      res.render("index.hbs",{usuarios})
+})
+router.get("/login",(req,res)=>{
+     res.render("login.hbs")
 })
 router.post("/usuario/:id",async(req,res)=>{
      let buscar_usuario = await usuarios.find(usuario => usuario.cedula === req.params.id) 
