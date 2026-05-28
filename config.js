72ca0957040bf6d7d668065c3fb58ad349ba927f
frikingsoft <nicolas.barone1@gmail.com>
2026-05-28 17:44:20 -0300

    cambios 28-5

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import express from "express" // LLamamos al modulo de express
 import { fileURLToPath } from "url" // para obtener las rutas del archivo actual
 import { dirname } from "path" // para obtener las rutas del directorio actual
-
+import  hbs  from "hbs"
 const __filename = fileURLToPath(import.meta.url)// para obtener las rutas del archivo actual
 const __dirname = dirname(__filename) // para obtener las rutas del directorio actual
 
@@ -11,6 +11,7 @@ servidor.listen(4000)//El puerto del servidor
 servidor.use(express.json())// configuramos el servidor para trabajar con json
 servidor.use(express.static(ruta))//definimos las rutas estaticas
 servidor.set("view engine","hbs")
+hbs.registerPartials(`${__dirname}/views/partials`)
 const router = express.Router()
 servidor.use(router)
 
